803cfdc8e447faf6610908e3b2c2b60f6932a434
unlsycn <unlsycn@unlsycn.com>
2024-08-03 22:40:56 +0800

    feat: support desktop and terminal applications
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- unlauncher.sh
+++ unlauncher.sh
@@ -3,12 +3,31 @@
 name=Unlauncher
 script_file=~/.scripts/bin/unlauncher
 frequency_file=~/.local/share/unlauncher/frequency
+apps_dir=/usr/share/applications
+
+parse_desktop_entry() {
+    while IFS= read -r line; do
+        [[ "$line" == "[Desktop Entry]" ]] && in_section=true && continue
+        [[ "$line" =~ ^\[.*\]$ ]] && in_section=false
+        $in_section && [[ "$line" =~ ^(Name|Exec|Terminal)= ]] && eval "${line%%=*}='${line#*=}'"
+    done <"$1"
+    echo -e "$(echo "$Exec" | awk '{print $1}' | xargs basename)\t$Name\t$Exec\t${Terminal:-false}"
+}
 
 launcher_cmd() {
-    {
+    selected_app=$({
         fre --store_name "${frequency_file}" --sorted
-        echo $PATH | tr ':' '\n' | xargs -n 1 ls
-    } | awk '!x[$0]++' | fzf --no-sort | tee >(xargs -n 1 fre --store_name "${frequency_file}" --add) | xargs -n 1 hyprctl dispatch exec
+        ls "${apps_dir}" | while IFS= read -r entry; do
+            parse_desktop_entry "${apps_dir}/${entry}"
+        done
+        echo $PATH | tr ':' '\n' | xargs -n 1 ls | awk '{print $0 "\t" $0 "\t" $0 "\ttrue"}'
+    } | awk -F'\t' '!y[$0]++ && (!x[$1]++ || $4 == "false")' | fzf --with-nth=2 --delimiter='\t' --no-sort)
+    if [ -n "${selected_app}" ]; then
+        echo "${selected_app}" | awk '{print "\"" $0 "\""}' | xargs fre --store_name "${frequency_file}" --add
+        exec=$(echo "${selected_app}" | cut -f 3)
+        command=$([ "$(echo "${selected_app}" | cut -f 4)" == "true" ] && echo "alacritty -e ${SHELL} -c '${exec} && read'" || echo "${exec}")
+        hyprctl dispatch exec "${command}"
+    fi
 }
 
 launcher_launch() {
